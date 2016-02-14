9c45a661475b4a1a20ac7446c6b1c7fdba2441f9
Ryan Petschek <petschekr@gmail.com>
2016-02-14 16:08:25 -0500

    Display schedule on home page from AJAX request

--- World Perspectives/routes/data.ts
+++ World Perspectives/routes/data.ts
@@ -21,7 +21,7 @@ var router = express.Router();
 
 router.route("/schedule").get(function (request, response) {
     db.cypher({
-		query: "MATCH (item:ScheduleItem) RETURN item.title AS title, item.time AS time, item.location AS location"
+		query: "MATCH (item:ScheduleItem) RETURN item.title AS title, item.time AS time, item.location AS location, item.editable AS editable"
 	}, function (err: Error, results) {
 		if (err) {
 			console.error(err);
