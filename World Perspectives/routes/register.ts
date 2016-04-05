154d35112313fc147f55b939e9e2ad6198b58b27
Ryan Petschek <petschekr@gmail.com>
2016-04-04 21:25:57 -0400

    Send selected sessions to register for to server

--- World Perspectives/routes/register.ts
+++ World Perspectives/routes/register.ts
@@ -3,6 +3,8 @@ var fs = Promise.promisifyAll(require("fs"));
 import common = require("../common");
 var db = common.db;
 import express = require("express");
+import bodyParser = require("body-parser");
+var postParser = bodyParser.json();
 var router = express.Router();
 var neo4j = require("neo4j");
 import moment = require("moment");
@@ -58,71 +60,77 @@ router.route("/sessions").get(function (request, response) {
 		response.json(results);
 	}).catch(common.handleError.bind(response));
 });
-router.route("/sessions/:time").get(function (request, response) {
-	var startTime = request.params.time;
-    db.cypherAsync({
-		query: `MATCH (s:Session {startTime: { startTime }}) RETURN
-			s.title AS title,
-			s.slug AS slug,
-			s.description AS description,
-			s.type AS type,
-			s.location AS location,
-			s.capacity AS capacity,
-			s.attendees AS attendees,
-			s.startTime AS startTime,
-			s.endTime AS endTime
-			ORDER BY lower(s.title)`,
-		params: {
-			startTime: startTime
-		}
-	}).then(function (results) {
-		return Promise.map(results, function (session: any) {
-			return db.cypherAsync({
-				queries: [{
-					query: "MATCH (user:User)-[r:PRESENTS]->(s:Session {slug: { slug }}) RETURN user.username AS username, user.name AS name ORDER BY last(split(user.name, \" \"))",
-					params: {
-						slug: session.slug
-					}
-				}, {
-					query: "MATCH (user:User)-[r:MODERATES]->(s:Session {slug: { slug }}) RETURN user.username AS username, user.name AS name",
-					params: {
-						slug: session.slug
-					}
-				}]
-			}).then(function (sessionRelationships) {
-				return {
-					"title": {
-						"formatted": session.title,
-						"slug": session.slug
-					},
-					"description": session.description,
-					"type": session.type,
-					"location": session.location,
-					"capacity": {
-						"total": session.capacity,
-						"filled": session.attendees
-					},
-					"time": {
-						"start": {
-							"raw": session.startTime,
-							"formatted": moment(session.startTime).format(timeFormat)
+router.route("/sessions/:time")
+	.get(function (request, response) {
+		var startTime = request.params.time;
+		db.cypherAsync({
+			query: `MATCH (s:Session {startTime: { startTime }}) RETURN
+				s.title AS title,
+				s.slug AS slug,
+				s.description AS description,
+				s.type AS type,
+				s.location AS location,
+				s.capacity AS capacity,
+				s.attendees AS attendees,
+				s.startTime AS startTime,
+				s.endTime AS endTime
+				ORDER BY lower(s.title)`,
+			params: {
+				startTime: startTime
+			}
+		}).then(function (results) {
+			return Promise.map(results, function (session: any) {
+				return db.cypherAsync({
+					queries: [{
+						query: "MATCH (user:User)-[r:PRESENTS]->(s:Session {slug: { slug }}) RETURN user.username AS username, user.name AS name ORDER BY last(split(user.name, \" \"))",
+						params: {
+							slug: session.slug
+						}
+					}, {
+						query: "MATCH (user:User)-[r:MODERATES]->(s:Session {slug: { slug }}) RETURN user.username AS username, user.name AS name",
+						params: {
+							slug: session.slug
+						}
+					}]
+				}).then(function (sessionRelationships) {
+					return {
+						"title": {
+							"formatted": session.title,
+							"slug": session.slug
 						},
-						"end": {
-							"raw": session.endTime,
-							"formatted": moment(session.endTime).format(timeFormat)
+						"description": session.description,
+						"type": session.type,
+						"location": session.location,
+						"capacity": {
+							"total": session.capacity,
+							"filled": session.attendees
 						},
-						"date": moment(session.startTime).format(dateFormat)
-					},
-					"presenters": sessionRelationships[0],
-					"moderator": (sessionRelationships[1].length !== 0) ? sessionRelationships[1][0] : null
-				};
+						"time": {
+							"start": {
+								"raw": session.startTime,
+								"formatted": moment(session.startTime).format(timeFormat)
+							},
+							"end": {
+								"raw": session.endTime,
+								"formatted": moment(session.endTime).format(timeFormat)
+							},
+							"date": moment(session.startTime).format(dateFormat)
+						},
+						"presenters": sessionRelationships[0],
+						"moderator": (sessionRelationships[1].length !== 0) ? sessionRelationships[1][0] : null
+					};
+				});
 			});
-		});
+		})
+		.then(function (sessions) {
+			response.json(sessions);
+		})
+		.catch(common.handleError.bind(response));
 	})
-	.then(function (sessions) {
-		response.json(sessions);
-	})
-	.catch(common.handleError.bind(response));
-});
+	.post(postParser, function (request, response) {
+		var {slug}: { slug: string } = request.body;
+		console.log(slug, response.locals.user.username);
+		response.json({ "success": true, "message": "Successfully registered for that session" });
+	});
 
 export = router;
